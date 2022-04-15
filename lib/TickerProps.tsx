7210826bcb4c942bed5c1bd746bd8d8f039aa170
g12i <grzebieniowski@gmail.com>
2022-04-15 18:14:37 +0200

    Fix props

--- lib/TickerProps.tsx
+++ lib/TickerProps.tsx
@@ -1,5 +1,7 @@
-export type TickerProps = {
+import type { PropsWithChildren } from "react";
+
+export type TickerProps = PropsWithChildren<{
   duration: KeyframeAnimationOptions["duration"];
   easing?: KeyframeAnimationOptions["easing"];
   delay?: KeyframeAnimationOptions["delay"];
-};
+}>;
