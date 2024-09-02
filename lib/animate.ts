8a79ac48a12e8fd77c2e17e79202aa8a7d9bef36
Wojciech Grzebieniowski <grzebieniowski@gmail.com>
2024-09-02 20:55:44 +0200

    Revet unnecessary changes

--- lib/animate.ts
+++ lib/animate.ts
@@ -1,23 +1,3 @@
-export function animationPromise(
-  animation: Animation,
-  signal: AbortSignal
-): Promise<void> {
-  return new Promise((resolve) => {
-    const onAbort = () => {
-      animation.cancel();
-    };
-
-    const onFinish = () => {
-      signal.removeEventListener("abort", onAbort);
-      animation.removeEventListener("finish", onFinish);
-      resolve();
-    };
-
-    signal.addEventListener("abort", onAbort);
-    animation.addEventListener("finish", onFinish);
-  });
-}
-
 export function animate(
   element: HTMLElement,
   keyframes: Keyframe[],
